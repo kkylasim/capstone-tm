1b10306f3c2318774bea0c4f613dcb33dc87d91c
nealetham <nealetham2000@gmail.com>
2025-10-13 21:23:28 +0800

    feat: add tenant, rule selection page

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./selection-page/selection-page').then(m => m.SelectionPageComponent)
+    }
+];

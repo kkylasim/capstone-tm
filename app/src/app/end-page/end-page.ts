132b53da9d8b17ba5904d45410ff8dc088b9c72c
Neale <nealetham@tamu.edu>
2025-10-14 16:17:19 +0800

    feat: add table to data generated page

--- app/src/app/end-page/end-page.ts
+++ app/src/app/end-page/end-page.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-end-page',
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatTableModule],
   templateUrl: './end-page.html',
   styleUrl: './end-page.scss'
 })
 export class EndPage {
   constructor(private router: Router) {}
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 
-  dataGenerated = "The basic building blocks of an Angular application are NgModules, which provide a compilation context for components. NgModules collect related code into functional sets; an Angular app is defined by a set of NgModules. An app always has at least a root module that enables bootstrapping, and typically has many more feature modules. This allows for better organization and separation of concerns within the application."
+  dataSource = [
+    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  ];
 
   onDownload() {
     console.log("Downloading data...");
